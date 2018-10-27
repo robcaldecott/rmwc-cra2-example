89a37985b72e804e45777df530605c9e131eff87
Rob Caldecott <robert.caldecott@gmail.com>
2018-10-27 12:35:17 +0100

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Button} from "@rmwc/button"
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@rmwc/button";
+import "./index.scss";
 
 function App() {
   return <Button raised>Hello, world!</Button>;
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
